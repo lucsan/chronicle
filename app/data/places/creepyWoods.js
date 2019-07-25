b4d279ef2f46e0d42d9c4a2f0f58b880f1516ce6
lucsan <1104946+lucsan@users.noreply.github.com>
2019-07-25 23:02:12 +0100

    🍔 Text change.

--- app/data/places/creepyWoods.js
+++ app/data/places/creepyWoods.js
@@ -1,5 +1,5 @@
 const creepyWoods_prose = `
-These dark dank woods are definatly creepy.
+These dark dank woods are definately creepy.
 <p>
 Creepy trees with creepy trunks with creepy branches and creepy creepy twigs
 creepily overhang a creepy creepy path which creeps and crawls through the
