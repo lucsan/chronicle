7c972fe706862b2db5e8d92a8fc1521cad0b703e
lucsan <1104946+lucsan@users.noreply.github.com>
2019-07-25 22:59:07 +0100

    🔨 Update to chronicle.

--- chronicle.js
+++ chronicle.js
@@ -1,20 +1,23 @@
 console.log('%cChronicle running ->>', 'color:red;font-weight:bold;')
 
 
-scriptsLoader(['app/config.js'], () => document.dispatchEvent(new Event('configLoaded')))
+scriptsLoader(['app/config.js'], () => configLoaded())
+const configLoaded = () => {
+  scriptsLoader(config.autoload, () => scriptsLoaded() )
+}
 
-document.addEventListener('configLoaded', () => {
-  scriptsLoader(config.autoload, () => document.dispatchEvent(new Event('scriptsLoaded')) )
-})
+//scriptsLoader(['app/config.js'], () => document.dispatchEvent(new Event('configLoaded')))
+  //scriptsLoader(config.autoload, () => document.dispatchEvent(new Event('scriptsLoaded')) )
+// document.addEventListener('configLoaded', () => {
+//   scriptsLoader(config.autoload, () => document.dispatchEvent(new Event('scriptsLoaded')) )
+// })
 
-let custom = () => {}
-
-document.addEventListener('scriptsLoaded', () => {
+const scriptsLoaded = () => {
   let css = 'color:green;'
   let css1 = 'font-weight:bold;'
-  console.log('%c--> Scripts Loaded', css)
+  console.info('%c--> Scripts Loaded', css)
 
-  console.log('%cMarshalling assets', css+css1)
+  console.info('%cMarshalling assets', css+css1)
   const marshall = marshalls(propsPlans, setsPlans)
 
   // scripted actions
@@ -33,11 +36,46 @@ document.addEventListener('scriptsLoaded', () => {
   const stage = theater(royal(marshall.cabinet.draws))
   stage.build()
 
-  marshall.move(marshall.cabinet.draws.character.location)
+  //marshall.move(marshall.cabinet.draws.character.location)
 
   //moves_test_saves(marshall)
   //moves_one(marshall)
 
-  console.log(marshall.cabinet)
+  //console.log(marshall.cabinet)
+}
 
-})
+
+let custom = () => {}
+
+// document.addEventListener('scriptsLoaded', () => {
+//   let css = 'color:green;'
+//   let css1 = 'font-weight:bold;'
+//   console.info('%c--> Scripts Loaded', css)
+//
+//   console.info('%cMarshalling assets', css+css1)
+//   const marshall = marshalls(propsPlans, setsPlans)
+//
+//   // scripted actions
+//   const customActions = (marshall, act) => {
+//     return (act) => {
+//     console.log('chra',act)
+//       marshall.action(act.action, act.id)
+//     }
+//   }
+//
+//   custom = customActions(marshall)
+//
+//   localStorage.setItem('player', JSON.stringify({ name: 'Playe Rone', character: 'Charac Ter' }))
+//   loadGame(marshall.cabinet)
+//
+//   const stage = theater(royal(marshall.cabinet.draws))
+//   stage.build()
+//
+//   marshall.move(marshall.cabinet.draws.character.location)
+//
+//   //moves_test_saves(marshall)
+//   //moves_one(marshall)
+//
+//   console.log(marshall.cabinet)
+//
+// })
