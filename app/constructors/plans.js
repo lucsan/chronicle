a52790e7fc4df7c22e0bbad0effc5b4c97d7a5b6
lucsan <1104946+lucsan@users.noreply.github.com>
2019-07-26 12:56:21 +0100

    🔨 Raises plans and scopes agains global.

--- app/constructors/plans.js
+++ app/constructors/plans.js
@@ -1,76 +1,75 @@
 console.log('--> plans')
 
-let plans = {
-  props: {},
-  sets: {},
-  codes: (obj) => {
+const plans = () => {
+  let props = {}
+  let sets = {}
+
+  const codes = (obj) => {
     let a = []
     for (let c in obj) { a.push(c) }
     return a
-  },
-  find: (c, objs) => { return objs[c] },
-}
+  }
 
-plans.loadProps = (data) => plans.props = data
-plans.loadSets = (data) => plans.sets = data
+  const propsAtLoc = (loc, decor) => {
+    let codes = Object.keys(decor)
+    let ids = []
+    codes.map(c => {
+      if (decor[c].locs) {
+        decor[c].locs.map(l => {
+          if ( l == loc) ids.push(c)
+        })
+      }
+    })
+    return ids
+  }
 
-plans.codesProps = () => plans.codes(plans.props)
-plans.codesSets = () => plans.codes(plans.sets)
 
-plans.propsAtLoc = (loc, decor) => {
-  let codes = Object.keys(decor)
-  let ids = []
-  codes.map(c => {
-    if (decor[c].locs) {
-      decor[c].locs.map(l => {
-        if ( l == loc) ids.push(c)
-      })
+  const initaliseProps = (defaultActions, action) => {
+    for (let id in props) {
+      let prop = props[id]
+      if (prop.code == undefined) prop.code = id
+      if (prop.desc == undefined) prop.desc = id
+      if (prop.locs == undefined) prop.locs = []
+      prop.usedIn = []
+      applyDefaultActions(prop, defaultActions(id, action))
     }
-  })
-  return ids
-}
-
-// plans.propsAtLoc = (loc, decor) => {
-//   let codes = Object.keys(decor)
-//   return codes.filter(c => {
-//     if (decor[c].locs) return decor[c].locs.find(l => l == loc)
-//   })
-// }
-
-plans.initaliseProps = (defaultActions, action) => {
-  for (let id in plans.props) {
-    let prop = plans.props[id]
-    if (prop.code == undefined) prop.code = id
-    if (prop.desc == undefined) prop.desc = id
-    if (prop.locs == undefined) prop.locs = []
-    prop.usedIn = []
-    applyDefaultActions(prop, defaultActions(id, action))
-  }
-  for (let id in plans.props) {
-    let prop = plans.props[id]
-    applyCombinesWith(prop)
-  }
-  return plans.props
-}
-
-const applyCombinesWith = (prop) => {
-  if (!prop.combines) return
-  prop.combines.needs.map(code => {
-    plans.props[code].usedIn.push(prop.code)
-  })
-}
-
-const applyDefaultActions = (prop, actions) => {
-  if (prop.actions === undefined) {
-    prop.actions = actions
-    if (!prop.pickUp) delete prop.actions.env.pickUp
-    return
-  }
-  for (let bag in actions) {
-    if (prop.actions[bag] === undefined) prop.actions[bag] = actions[bag]
-    for (let act in actions[bag]) {
-      if (act == 'pickUp' && !prop.pickUp) continue
-      if (prop.actions[bag][act] === undefined) prop.actions[bag][act] = actions[bag][act]
+    for (let id in props) {
+      let prop = props[id]
+      applyCombinesWith(prop)
     }
+    return props
+  }
+
+  const applyCombinesWith = (prop) => {
+    if (!prop.combines) return
+    prop.combines.needs.map(code => {
+      props[code].usedIn.push(prop.code)
+    })
+  }
+
+  const applyDefaultActions = (prop, actions) => {
+    if (prop.actions === undefined) {
+      prop.actions = actions
+      if (!prop.pickUp) delete prop.actions.env.pickUp
+      return
+    }
+    for (let bag in actions) {
+      if (prop.actions[bag] === undefined) prop.actions[bag] = actions[bag]
+      for (let act in actions[bag]) {
+        if (act == 'pickUp' && !prop.pickUp) continue
+        if (prop.actions[bag][act] === undefined) prop.actions[bag][act] = actions[bag][act]
+      }
+    }
+  }
+
+
+  return {
+    loadProps: (data) => { props = data },
+    loadSets: (data) => { sets = data },
+    codesProps: () => { codes(props) },
+    codesSets: () => { codes(sets) },
+    propsAtLoc,
+    initaliseProps,
+
   }
 }
--- app/game/marshalls.js
+++ app/game/marshalls.js
@@ -1,23 +1,23 @@
 const marshalls = (defaultProps, defaultSets) => {
   let cabinet = new Cabinet
   let acts = actions()
+  let planz = plans()
 
   const move = (to) => acts.doMove(to, cabinet)
   const action = (act, id) => acts.doAction(act, id, cabinet)
   // const custom = (act, id, special) => {
   //   acts.doCustom(act, id, special, cabinet)
   // }
-
   cabinet.use({ props: defaultProps })
   cabinet.use({ sets: defaultSets })
-
-  plans.loadProps(cabinet.draws.props)
-  cabinet.use({ decor: plans.initaliseProps(acts.defaultActions, action) })
-
-  plans.loadSets(cabinet.draws.sets)
   cabinet.use({ places: defaultSets })
 
-  cabinet.use({ tools: { propsAtLoc: plans.propsAtLoc } })
+  planz.loadProps(cabinet.draws.props)
+  planz.loadSets(cabinet.draws.sets)
+
+  cabinet.use({ decor: planz.initaliseProps(acts.defaultActions, action) })
+  cabinet.use({ tools: { propsAtLoc: planz.propsAtLoc } })
+
   cabinet.use({ tools: { move: move } })
   cabinet.use({ tools: { saveGame: saveGame } } )
   cabinet.use({ tools: { loadGame: loadGame } } )
