320f10d41649b585641cc6e882613707d032bb55
lucsan <1104946+lucsan@users.noreply.github.com>
2019-07-31 17:40:31 +0100

    🔨 Fixes up plansSpec.

--- spec/plansSpec.js
+++ spec/plansSpec.js
@@ -1,6 +1,6 @@
 describe('plans', () => {
   //let cabinet = new Cabinet
-  let plan = plans()
+  let planz = plans()
   let testObj = {
     test1: {
       desc: 'test prop one',
@@ -19,35 +19,17 @@ describe('plans', () => {
   }
 
   it('should have a plans object', () => {
-    expect(typeof(plan)).toBe('object')
+    expect(typeof(planz)).toBe('object')
   })
 
-  it('should have a props load function', () => {
-    plan.loadProps(testObj)
-    expect(plan.props).toBe(testObj)
-  })
 
-  xit('should load sets', () => {
-    plan.loadSets(testObj)
-    expect(plan.sets).toBe(testObj)
-  })
+  it('should initialise props (ie: defaults)', () => {
+    let decor = planz.initaliseProps(testObj, {
+      defaultActions: defaultActions,
+      action: () => {},
+      draws: []
+    })
 
-  xit('should provide props codes', () => {
-    plan.loadProps(testObj)
-    expect(plan.codesProps().length > 1).toBe(true)
-  })
-
-  xit('should return list of props at a location', () => {
-    let propsAtTest = plans.propsAtLoc('test', testObj)
-    expect(propsAtTest[1]).toBe('test3')
-  })
-
-  xit('should initialise props (ie: defaults)', () => {
-    //let m = marshalls(testObj, testObj)
-    //console.log(m)
-
-    plan.loadProps(testObj)
-    let decor = plans.initaliseProps(defaultActions)
     expect(typeof(decor.test1.actions.env.lick)).toBe('function')
   })
 
